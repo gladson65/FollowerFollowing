bb52577f51d58c93ecbcfdb0acddc8a40d27d2d4
gladson65 <grauth65@gmail.com>
2025-10-08 17:22:06 +0530

    follow function has been implemented

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -21,3 +21,32 @@ export function createUser(req, res) {
         return res.status(500).json({error: error.message});
     })
 }
+
+// add follower
+export async function followUser(req, res) {
+    const { userID, targetID } = req.body;
+
+    // key validation
+    if (!userID || !targetID) return res.status(400).json({message: 'userID and targetID both are required.'});
+
+    // if follower and the following ID same
+    if (userID === targetID) return res.status(400).json({message: 'cannot follow yourself'});
+
+    // find user and target user by ID
+    const user = await userModel.findById(userID);
+    const target = await userModel.findById(targetID);
+
+    // if they not found
+    if (!user || !target) return res.status(404).json({message: "user not found"})
+
+    // check if the targetID already there or not then add
+    if (!user.following.includes(targetID)) {
+        user.following.push(targetID);
+        target.followers.push(userID);
+
+        await user.save()
+        await target.save()
+    }
+
+    return res.status(200).json({message: `${user.username} followed ${target.username}`});
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
-import { createUser } from "../controllers/user.controllers.js"
+import { createUser, followUser } from "../controllers/user.controllers.js"
 
 export function userRoutes(followerServer) {
-    followerServer.post('/user', createUser)
+    followerServer.post('/user', createUser),
+    followerServer.post('/follow', followUser)
 }
